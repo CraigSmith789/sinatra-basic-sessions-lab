bb90c5febf43970a2aebc28e95043bfdb01546cd
CraigSmith789 <craig789@gmail.com>
2020-12-03 14:43:27 -0500

    commit tree aa5a033cfa0e2ca879aa98b0ecf729680fc53923

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+  enable :sessions
+  set :session_secret, "Chelsea"
+  end
+
+  get '/'  do
+    erb :index    
+  end
+
+  
+
+  post '/checkout' do 
+    session[:item] = params[:item]
+    @session = session
+    # binding.pry
+    erb :checkout
+  end
+
+
 end
